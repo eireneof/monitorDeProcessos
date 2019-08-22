4ccfbbc58d64c009c91e45299170ffe6b8542079
Samuel Lucas <slvlb@ic.ufal.br>
2019-08-22 16:20:17 -0300

    Adicionando graficos

--- src/main.c
+++ src/main.c
@@ -11,14 +11,13 @@
 #define ERROR -1
 #define DEBUG(x) if(x)
 
-void cpu()
+void cpu() // Tests CPU;
 {
 	for(;;)
-	{ 
-	}
+	{}
 }
 
-void cpu_mem()
+void cpu_mem() // Tests CPU and memory;
 { 
 	for(;;)
 	{ 
@@ -26,19 +25,17 @@ void cpu_mem()
 	}
 }
 
-int string_compare(int i, char str_1[], char str_2[])
+int string_compare(int i, char str_1[], char str_2[]) // Compares two strings
 {
-	if(str_1[i] == '\0' && str_2[i] == '\0')
-	{
-		return 0;
-	}
+	if(str_1[i] == '\0' && str_2[i] == '\0') return 0; // If it reaches the end, the function returns 0;
+
 	else
 	{
-		if(str_1[i] != str_2[i])
+		if(str_1[i] != str_2[i]) // If it find two different letters;
 		{
 			return 1;
 		}
-		else
+		else // Else, it will look for the next letters
 		{
 			return string_compare(i + 1, str_1, str_2);
 		}
@@ -47,30 +44,27 @@ int string_compare(int i, char str_1[], char str_2[])
 
 int main (int argc, char *argv[], char *envp[])
 {
-	int pid = fork();
+	int pid = fork(); // Creates a new process;
+	char cmd[MAX], id[MAX]; // "cmd" stores the bash command and "id", the process ID;
 
-	char cmd[MAX], id[MAX];
-
-    int i;
-
-	if (pid <= ERROR)
+	if (pid <= ERROR) // if creating a process didn't work
 	{
 		perror ("Error: ");
 		exit (ERROR);
 	}
-	else if(pid >= PARENT_ID) // I'm the father process
+	else if(pid >= PARENT_ID) // If I'm the parent process
 	{
 		sprintf(id, "%d", pid);
-		if(string_compare(0, argv[1], "ucp") == 0)
+		if(string_compare(0, argv[1], "ucp") == 0) // If argv[1] == "ucp"
 		{
-            printf("==== CPU TEST ====\n");
+            printf("==== CPU TEST ====\n"); // Print a header
 			printf(" PID - %%CPU\n");
 			strcpy(cmd, "ps -e -o pid,pcpu | grep ");
 			strcat(cmd, id);
 		}
-		else if(string_compare(0, argv[1], "ucp_mem") == 0)
+		else if(string_compare(0, argv[1], "ucp_mem") == 0) // If argv[1] == "ucp_mem"
 		{ 
-            printf("==== CPU & MEMORY TEST ====\n");
+            printf("==== CPU & MEMORY TEST ====\n"); // Print a header
 			printf(" PID - %%CPU\n");
 			strcpy(cmd, "ps -e -o pid,pcpu | grep ");
 			strcat(cmd, id);
@@ -79,17 +73,17 @@ int main (int argc, char *argv[], char *envp[])
 			strcat(cmd, " | grep -i Total");
 		}
 
-		for(i = 0; i < 1; i++)
+		for(int i = 0; i < 1; i++) // 
 		{
 			system(cmd);
-			sleep(1);
+			sleep(1); // Wait one second;
 		}
 
 		strcpy(cmd, "kill ");
 		strcat(cmd, id);
-		system(cmd);
+		system(cmd); // Kill the child process
 	}
-	else //pid == CHILD_ID
+	else // If I'm the child (pid == CHILD_ID)
 	{
 		if(string_compare(0, argv[1], "ucp") == 0)
 		{
