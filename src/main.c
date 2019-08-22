c3066b5f8e2220dc875c0c1542d717375e05d65d
eireneof <52976035+eireneof@users.noreply.github.com>
2019-08-22 14:33:14 -0300

    Ajeitando o código

--- src/main.c
+++ src/main.c
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX 500
+#define CHILD_ID 0
+#define PARENT_ID 1
+#define ERROR -1
+#define DEBUG(x) if(x)
+
 void cpu()
 {
 	for(;;)
@@ -41,44 +47,49 @@ int string_compare(int i, char str_1[], char str_2[])
 
 int main (int argc, char *argv[], char *envp[])
 {
-	int pid = fork (
+	int pid = fork();
 
-	char sys[100], aux[100];
+	char cmd[MAX], id[MAX];
 
-	if ( pid < 0 )
+    int i;
+
+	if (pid <= ERROR)
 	{
-		perror ("Erro: ") ;
-		exit (-1) ;
+		perror ("Error: ");
+		exit (ERROR);
 	}
-	else if( pid > 0 ) // I'm the father process
+	else if(pid >= PARENT_ID) // I'm the father process
 	{
-		sprintf(aux, "%d", pid);
+		sprintf(id, "%d", pid);
 		if(string_compare(0, argv[1], "ucp") == 0)
 		{
-			printf(" PID - CPU%%\n");
-			strcpy(sys, "ps -e -o pid,pcpu | grep ");
-			strcat(sys, aux);
+            printf("==== CPU TEST ====\n");
+			printf(" PID - %%CPU\n");
+			strcpy(cmd, "ps -e -o pid,pcpu | grep ");
+			strcat(cmd, id);
 		}
-
 		else if(string_compare(0, argv[1], "ucp_mem") == 0)
 		{ 
-			printf(" PID - CPU%%\n");
-			strcpy(sys, "ps -e -o pid,pcpu | grep ");
-			strcat(sys, aux);
-			strcat(sys, ";pmap ");
-			strcat(sys, aux); 
-			strcat(sys, " | grep -i total");
+            printf("==== CPU & MEMORY TEST ====\n");
+			printf(" PID - %%CPU\n");
+			strcpy(cmd, "ps -e -o pid,pcpu | grep ");
+			strcat(cmd, id);
+			strcat(cmd, ";pmap ");
+			strcat(cmd, id); 
+			strcat(cmd, " | grep -i Total");
 		}
-		for(int i = 0; i < 1; i++)
+
+		for(i = 0; i < 1; i++)
 		{
-			system(sys);
+			system(cmd);
 			sleep(1);
 		}
-		strcpy(sys, "kill ");
-		strcat(sys, aux);
-		system(sys);
+
+		strcpy(cmd, "kill ");
+		strcat(cmd, id);
+		system(cmd);
 	}
-	else 
+	else //pid == CHILD_ID
 	{
 		if(string_compare(0, argv[1], "ucp") == 0)
 		{
@@ -89,8 +100,8 @@ int main (int argc, char *argv[], char *envp[])
 			cpu_mem();
 		}
 	}
-	perror ("Erro: ") ; /* execve did not work */
+	perror ("Error: "); /* execve did not work */
 
-	printf ("Tchau !\n") ;
-	exit(0) ; /* Terminates the process successfully (code 0) */
+	printf ("Tchau !\n");
+	exit(0); /* Terminates the process successfully (code 0) */
 }
