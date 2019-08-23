2e203af92ef0f12a8691574bf98c5d77e606874a
Samuel Lucas <slvlb@ic.ufal.br>
2019-08-23 15:45:07 -0300

    Comentando o código em inglês

--- src/main.c
+++ src/main.c
@@ -47,49 +47,49 @@ int main (int argc, char *argv[], char *envp[])
 	int pid = fork(); // Creates a new process;
 	char cmd[MAX], id[MAX]; // "cmd" stores the bash command and "id", the process ID;
 
-	if (pid <= ERROR) // if creating a process didn't work
+	if (pid <= ERROR) // if creating a child process didn't work
 	{
 		perror ("Error: ");
 		exit (ERROR);
 	}
 	else if(pid >= PARENT_ID) // If I'm the parent process
 	{
-		sprintf(id, "%d", pid);
+		sprintf(id, "%d", pid); //Converts the PID (int) into a string
 		if(string_compare(0, argv[1], "ucp") == 0) // If argv[1] == "ucp"
 		{
             printf("==== CPU TEST ====\n"); // Print a header
-			printf(" PID - %%CPU\n");
-			strcpy(cmd, "ps -e -o pid,pcpu | grep ");
-			strcat(cmd, id);
+			printf(" PID - %%CPU\n"); // Print another header
+			strcpy(cmd, "ps -e -o pid,pcpu | grep "); // Saves the command in the string "cmd", using "grep" as a filter
+			strcat(cmd, id); // Concatenates the process ID
 		}
 		else if(string_compare(0, argv[1], "ucp_mem") == 0) // If argv[1] == "ucp_mem"
 		{ 
             printf("==== CPU & MEMORY TEST ====\n"); // Print a header
-			printf(" PID - %%CPU\n");
-			strcpy(cmd, "ps -e -o pid,pcpu | grep ");
-			strcat(cmd, id);
-			strcat(cmd, ";pmap ");
-			strcat(cmd, id); 
+			printf(" PID - %%CPU\n");// Print another header
+			strcpy(cmd, "ps -e -o pid,pcpu | grep "); // Saves the command in the string "cmd", using "grep" as a filter
+			strcat(cmd, id); // Concatenates the process ID
+			strcat(cmd, ";pmap "); // Concatenates "pmap", wich is used for show the memory usage
+			strcat(cmd, id); // Concatenates the process ID
 			strcat(cmd, " | grep -i Total");
 		}
 
-		for(int i = 0; i < 1; i++) // 
+		for(int i = 0; i < 1; i++) // Executes the command during 10 seconds
 		{
-			system(cmd);
+			system(cmd); // Executes the command
 			sleep(1); // Wait one second;
 		}
 
-		strcpy(cmd, "kill ");
+		strcpy(cmd, "kill "); // "kill" is used for kill the child process
 		strcat(cmd, id);
 		system(cmd); // Kill the child process
 	}
 	else // If I'm the child (pid == CHILD_ID)
 	{
-		if(string_compare(0, argv[1], "ucp") == 0)
+		if(string_compare(0, argv[1], "ucp") == 0) // If argv[1] == "ucp"
 		{
             cpu();
 		}
-		else if(string_compare(0, argv[1], "ucp_mem") == 0)
+		else if(string_compare(0, argv[1], "ucp_mem") == 0) // If argv[1] == "ucp_mem"
 		{
 			cpu_mem();
 		}
